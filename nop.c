196af3ba8d66cc649152b67ec8b4559946ff7881
Zaineb-Hammouda <zaineb.hammouda.zee@gmail.com>
2023-06-25 22:01:59 +0200

    Revert "fixed nop function"
    This reverts commit fdee54ba848040e18d35860c72dbf0dc180aa493.

--- nop.c
+++ nop.c
@@ -10,10 +10,4 @@ void nop(stack_t **stack, unsigned int line_number)
 {
 	(void)stack;
 	(void)line_number;
-	if (glo.args[1])
-	{
-		fprintf(stderr, "Usage: nop\n");
-		_free();
-		exit(EXIT_FAILURE);
-	}
 }
--- opcodes/nop.m
+++
@@ -1,7 +0,0 @@
-push 1
-      push 2
-       push 0
-       pall
-   nop
-   pall
-   line
